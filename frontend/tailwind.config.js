430da4cb5e40a6ccec7b60c8cde42052415600d5
Dileep01712 <dy3398214@gmail.com>
2024-12-08 11:43:33 +0530

    Fixed some issues in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -11,39 +13,38 @@ export default {
 	prefix: "",
 	theme: {
 		container: {
-			center: 'true',
+			center: true,
 			padding: '2rem',
 			screens: {
-				'2xl': '1400px'
-			}
+				'2xl': '1400px',
+			},
 		},
 		extend: {
 			textColor: {
-				gradient: 'linear-gradient(to right, #ffffff, #a855f7, #3b82f6)'
+				gradient: 'linear-gradient(to right, #ffffff, #a855f7, #3b82f6)',
 			},
 			backgroundImage: {
 				'instagram-gradient': 'linear-gradient(35deg, #f9ce34, #fa7e1e, #d62976, #962fbf, #4f5bd5)',
 				'instagram-gradient-light': 'linear-gradient(35deg, #fcd950, #fa851e, #d23976, #a033cc, #4F5BF1)',
-				
 			},
 			transitionProperty: {
-				top: 'top'
+				top: 'top',
 			},
 			transitionDuration: {
 				'300': '300ms',
 				'500': '500ms',
 				'700': '700ms',
-				'1000': '1000ms'
+				'1000': '1000ms',
 			},
 			transitionTimingFunction: {
-				'ease-in-out': 'ease-in-out'
+				'ease-in-out': 'ease-in-out',
 			},
 			spacing: {
-				'18': '4.5rem'
+				'18': '4.5rem',
 			},
 			boxShadow: {
 				transparent: '0 1px 2px 0 rgba(0, 0, 0, 0)',
-				'top-custom': '`0 -15px 10px -2px rgba(0, 0, 0, 0.5)`'
+				'top-custom': '`0 -15px 10px -2px rgba(0, 0, 0, 0.5)`',
 			},
 			colors: {
 				bodyColor: '#212121',
@@ -58,59 +59,59 @@ export default {
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
+					foreground: 'hsl(var(--primary-foreground))',
 				},
 				secondary: {
 					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
+					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
+					foreground: 'hsl(var(--destructive-foreground))',
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
+					foreground: 'hsl(var(--muted-foreground))',
 				},
 				accent: {
 					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
+					foreground: 'hsl(var(--accent-foreground))',
 				},
 				popover: {
 					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
+					foreground: 'hsl(var(--popover-foreground))',
 				},
 				card: {
 					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
-				}
+					foreground: 'hsl(var(--card-foreground))',
+				},
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				sm: 'calc(var(--radius) - 4px)',
 			},
 			keyframes: {
 				shimmer: {
 					'100%': {
-						transform: 'translateX(100%)'
-					}
+						transform: 'translateX(100%)',
+					},
 				},
 				'accordion-down': {
 					from: {
-						height: '0'
+						height: '0',
 					},
 					to: {
-						height: 'var(--radix-accordion-content-height)'
-					}
+						height: 'var(--radix-accordion-content-height)',
+					},
 				},
 				'accordion-up': {
 					from: {
-						height: 'var(--radix-accordion-content-height)'
+						height: 'var(--radix-accordion-content-height)',
 					},
 					to: {
-						height: '0'
-					}
+						height: '0',
+					},
 				},
 				'gradient-x': {
 					'0%, 100%': { 'background-position': 'left center' },
@@ -121,18 +122,17 @@ export default {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
 				'gradient-x': 'gradient-x 15s ease infinite',
-			}
-		}
+			},
+		},
 	},
 	plugins: [
-		// eslint-disable-next-line no-undef
-		require("tailwindcss-animate"),
+		tailwindcssAnimate,
 		function ({ addBase }) {
 			addBase({
 				// For Chrome, Safari, Edge, Opera
 				'input[type="number"]::-webkit-outer-spin-button, input[type="number"]::-webkit-inner-spin-button': {
 					'-webkit-appearance': 'none',
-					'margin': '0',
+					margin: '0',
 				},
 				// For Firefox
 				'input[type="number"]': {
@@ -140,15 +140,17 @@ export default {
 				},
 			});
 		},
-
 		function ({ addUtilities }) {
-			addUtilities({
-				'.text-gradient': {
-					background: 'linear-gradient(to right, #ffffff, #7e1ddb, #3b82f6)',
-					'-webkit-background-clip': 'text',
-					'-webkit-text-fill-color': 'transparent',
+			addUtilities(
+				{
+					'.text-gradient': {
+						background: 'linear-gradient(to right, #ffffff, #7e1ddb, #3b82f6)',
+						'-webkit-background-clip': 'text',
+						'-webkit-text-fill-color': 'transparent',
+					},
 				},
-			}, ['responsive', 'hover'])
-		}
+				['responsive', 'hover']
+			);
+		},
 	],
-}
+};
