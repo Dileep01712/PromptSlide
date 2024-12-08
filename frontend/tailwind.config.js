ecbafaf5e1da94e84e1e79a7d1d49539fedb4bb3
Dileep01712 <dy3398214@gmail.com>
2024-12-08 11:29:25 +0530

    Fixed some issues in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
+
+export default {
 	darkMode: ["class"],
 	content: [
 		'./pages/**/*.{ts,tsx}',
